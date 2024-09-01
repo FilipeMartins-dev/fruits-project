5fc735e5b8d1b6f548375c197157657449e41d21
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-08-31 22:27:36 -0300

    Criando arquivo JS separado para os componentes da página
    obejtivo: contribuição para a branch de refatoração em módulos JS

---
+++ components.mjs
@@ -0,0 +1,11 @@
+export const body = document.querySelector('body')
+
+export const containerStrawberry = document.querySelector('#container-strawberry')
+export const containerAvocado = document.querySelector('#container-avocado')
+export const containerOrange = document.querySelector('#container-orange')
+export const container = document.querySelectorAll('.container')
+
+export const buttonPrevious = document.querySelector('#button-previous')
+export const buttonNext = document.querySelector('#button-next')
+
+export const themes = [containerStrawberry, containerAvocado, containerOrange]
